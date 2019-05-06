3747d7dd6ac1eea86fa16039838acd1a7b76621f
marinakeu <marina_keunecke@hotmail.com>
2019-05-06 16:58:28 -0300

    master - refatora e inclui readme

---
+++ README.md
@@ -0,0 +1,19 @@
+# Extrair links de Markdown v.1.0.0
+
+**Esta biblioteca se destina à extrair links de arquivos markdown.**  
+
+## Como instalar:
+
+$  npm install linksmarkdown-mk
+
+## Como utilizar:
+
+    > const linksMd = require("linksmarkdown-mk");
+    > console.log(linksMd.getLinksFromMd('ut aliquip ex ea commodo consequat. [foo](http://foo.com) Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.'))
+    > // returns [{ href:  'http://foo.com', text:  'foo' }];
+
+## roadmap oficial do projeto
+
+#### versão 1.0.0 (released)
+
+-   funcionalidades: extração de links em arquivos markdown.
--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-function getLinksFromMd(str) {
-    let texts =  str.match(/(?<=\[).+?(?=\])/g);
-    let hrefs = str.match(/(?<=\()(.+?)(?=\))/g);
-    let array = [];
-    if (texts) {
-        texts.forEach((element, index) => array.push({ href: hrefs[index], text: element}));
-    };    
-    return array;
-  };
+function getLinksFromMd (str) {
+  let texts = str.match(/(?<=\[).+?(?=(\]\())/g);
+  let hrefs = str.match(/(?<=\]\()(.+?)(?=\))/g);
+  let array = [];
+  if (texts && texts.length === hrefs.length) {
+    texts.forEach((element, index) => array.push({ href: hrefs[index],
+      text: element}));
+  }    
+  return array;
+}
 
 module.exports.getLinksFromMd = getLinksFromMd;
